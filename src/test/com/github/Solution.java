2d2f223641b2fb103b4ffce0a0fdef92148f3f87
biyanwen <biyw@tsintergy.com>
2022-05-21 14:50:06 +0800

    feat:[102]二叉树的层序遍历

--- src/test/com/github/Solution.java
+++ src/test/com/github/Solution.java
@@ -2,23 +2,61 @@ package com.github;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
     @Test
-    public void test() {
-        int reverse = reverse(-1);
-        System.out.println(reverse);
+    public void tt() {
+        TreeNode treeNode = new TreeNode(3, new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        List<List<Integer>> lists = levelOrder(treeNode);
+        System.out.println(lists);
     }
 
-    public int reverse(int x) {
-        int rev = 0;
-        while (x != 0) {
-            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
-                return 0;
-            }
-            int digit = x % 10;
-            x /= 10;
-            rev = rev * 10 + digit;
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
         }
-        return rev;
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmpList = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                tmpList.add(poll.val);
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            result.add(tmpList);
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }

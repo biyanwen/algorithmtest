ffa2fe205db2674b9e53424185ae6d4915878e9b
biyanwen <biyw@tsintergy.com>
2022-05-30 13:11:25 +0800

    feat:[45]跳跃游戏 II

--- src/test/com/github/Solution.java
+++ src/test/com/github/Solution.java
@@ -11,24 +11,6 @@ class Solution {
                 new TreeNode(2, new TreeNode(3), new TreeNode(4)));
 
 
-        boolean symmetric = isSymmetric(treeNode);
-        System.out.println(symmetric);
-    }
-
-
-    public boolean isSymmetric(TreeNode root) {
-        return check(root, root);
-    }
-
-    private boolean check(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
-            return true;
-        }
-        if (left == null || right == null) {
-            return false;
-        }
-        return left.val == right.val && check(left.left, right.right)
-                && check(left.right, right.left);
     }
 
     class Node {
